b40deb56757db15f4842ba9125c4f6682862f82f
Jean-Marc Fontaine <jm@jmfontaine.net>
2017-07-14 00:05:14 +0200

    Add basic layout
    # Conflicts:
    #	package.json

--- build.js
+++ build.js
@@ -1,10 +1,20 @@
+const layouts = require('metalsmith-layouts')
 const markdown = require('metalsmith-markdown')
 const metalsmith = require('metalsmith')
 
 metalsmith(__dirname)
+  .metadata({
+    site: {
+      title: 'Jean-Marc Fontaine'
+    }
+  })
   .source('./src')
   .destination('./build')
   .use(markdown())
+  .use(layouts({
+    default: 'layout.html',
+    engine: 'ejs'
+  }))
   .build(function (err) {
     if (err) {
       throw err
--- src/posts/2017/07/13/reboot.md
+++ src/posts/2017/07/13/reboot.md
@@ -1,1 +1,4 @@
+---
+title: Reboot
+---
 This is a reboot of this blog in English.
