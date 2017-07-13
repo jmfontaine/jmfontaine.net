f3a54e50907c43d2653612f17d832b3195ad8d87
Jean-Marc Fontaine <jm@jmfontaine.net>
2017-07-14 00:04:42 +0200

    Use Markdown as markup language
    # Conflicts:
    #	package.json

--- build.js
+++ build.js
@@ -1,8 +1,10 @@
+const markdown = require('metalsmith-markdown')
 const metalsmith = require('metalsmith')
 
 metalsmith(__dirname)
   .source('./src')
   .destination('./build')
+  .use(markdown())
   .build(function (err) {
     if (err) {
       throw err
